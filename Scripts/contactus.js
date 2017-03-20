b1fcb136a9c9c3c7e41c49a73013070cfc1c9480
K1613345 <k1613345@kingston.ac.uk>
2017-03-20 23:14:04 +0000

    Upload new file

---
+++ Scripts/contactus.js
@@ -0,0 +1,23 @@
+
+function validate() {
+
+    if (!document.ContactUs.FirstName.value.match(/^[A-Za-z]{3,20}$/)) {
+        alert("Please provide Valid First Name!");
+        document.ContactUs.FirstName.focus();
+        return false;
+    }
+    if (!document.ContactUs.EmailAddress.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
+    {
+        alert("Please enter correct email ID")
+        document.ContactUs.EmailAddress.focus() ;
+        return false;
+    }
+
+    if( !document.ContactUs.Comments.value.match(/^[A-Za-z0-9]$/) )
+    {
+        alert( "Please provide Valid Password!" );
+        document.ContactUs.Comments.focus() ;
+        return false;
+    }
+
+}
