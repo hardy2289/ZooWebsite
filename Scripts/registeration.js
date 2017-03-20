bd505bb519f557b6600d08cc764516296d90dfcf
hardy <k1613345@kingston.ac.uk>
2017-03-20 20:04:19 +0000

    Add some extra features

--- Scripts/registeration.js
+++ Scripts/registeration.js
@@ -1,86 +1,61 @@
+/**
+ * Created by Ajay Bakari on 20/03/2017.
+ */
 
 
 function validate()
-{ 
-   if( document.Registration.textnames.value == "" )
-   {
-     alert( "Please provide your First Name!" );
-     document.Registration.textnames.focus() ;
-     return false;
-   }
-   if( document.Registration.lastname.value == "" )
-   {
-     alert( "Please provide your Last Name!" );
-     document.Registration.lastname.focus() ;
-     return false;
-   }
-   
-   if( document.Registration.dob.value == "" )
-   {
-     alert( "Please provide your DOB!" );
-     document.Registration.dob.focus() ;
-     return false;
-   }
-   
-   if ( ( Registration.sex[0].checked == false ) && ( Registration.sex[1].checked == false ) )
-   {
-   alert ( "Please choose your Gender: Male or Female" );
-   return false;
-   } 
-   
-   if( document.Registration.userid.value == "" )
-   {
-     alert( "Please provide User ID!" );
-     document.Registration.userid.focus() ;
-     return false;
-   }
-   if( document.Registration.password.value == "" )
-   {
-     alert( "Please provide Password!" );
-     document.Registration.password.focus() ;
-     return false;
-   }
-  
-   if( document.Registration.paddress.value == "" )
-   {
-     alert( "Please provide your Address!" );
-     document.Registration.paddress.focus() ;
-     return false;
-   }
-   if( document.Registration.City.value == "-1" )
-   {
-     alert( "Please provide your City!" );
-     document.Registration.City.focus() ;
-     return false;
-   }   
-   if( document.Registration.pincode.value == "" ||
-           isNaN( document.Registration.pincode.value) ||
-           document.Registration.pincode.value.length != 6 )
-   {
-     alert( "Please provide a Postcode in the format ######." );
-     document.Registration.pincode.focus() ;
-     return false;
-   }
-      
- var email = document.Registration.emailid.value;
-  atpos = email.indexOf("@");
-  dotpos = email.lastIndexOf(".");
- if (email == "" || atpos < 1 || ( dotpos - atpos < 2 )) 
- {
-     alert("Please enter correct email ID")
-     document.Registration.emailid.focus() ;
-     return false;
- }
-   
- if( document.Registration.mobileno.value == "" ||
-           isNaN( document.Registration.mobileno.value) ||
-           document.Registration.mobileno.value.length != 11 )
-   {
-     alert( "Please provide a Phone Number in 11 Digit Format." );
-     document.Registration.mobileno.focus() ;
-     return false;
-   }
- 
+{
+    if( !document.Registration.textnames.value.match(/^[A-Za-z]{3,20}$/) )
+    {
+        alert( "Please provide Valid First Name!" );
+        document.Registration.textnames.focus() ;
+        return false;
+    }
+    if( !document.Registration.lastname.value.match(/^[A-Za-z]{3,20}$/) )
+    {
+        alert( "Please provide Valid Last Name!" );
+        document.Registration.lastname.focus() ;
+        return false;
+    }
+
+
+    if( !document.Registration.password.value.match(/^[A-Za-z0-9!@#$%^&*()_]{6,20}$/) )
+    {
+        alert( "Please provide Password!" );
+        document.Registration.password.focus() ;
+        return false;
+    }
+
+    if( !document.Registration.number1.value.match(/^[0-9 ]{1,4}$/) )
+    {
+        alert( "Please provide Valid House Number!" );
+        document.Registration.number1.focus() ;
+        return false;
+    }
+
+    if( !document.Registration.pincode.value.match((/^[A-Za-z0-9]{7}$/)))
+    {
+        alert( "Please provide a Postcode in the format ######." );
+        document.Registration.pincode.focus() ;
+        return false;
+    }
+
+
+    if (!document.Registration.emailid.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
+    {
+        alert("Please enter correct email ID")
+        document.Registration.emailid.focus() ;
+        return false;
+    }
+
+    if( !document.Registration.mobileno.value.match(/^[0-9 ]{11}$/))
+
+    {
+        alert( "Please provide a Phone Number in 11 Digit Format." );
+        document.Registration.mobileno.focus() ;
+        return false;
+    }
+
 
 
 }
