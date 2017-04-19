26e40974ab0009690978058335ff42f1a95e5bd2
hardy <k1613345@kingston.ac.uk>
2017-04-19 01:12:04 +0100

    Update

--- builds/menageriezoo/js/information.js
+++ builds/menageriezoo/js/information.js
@@ -1,44 +1,11 @@
-/**
- * Created by Ajay Bakari on 13/04/2017.
- */
-
-
-// Initialize Firebase
-
-
-
 function answers() {
-    var answer = document.getElementById("mySelect");
 
-    if (answer[answer.selectedIndex].value == "Bonobo") {
 
-        var rootRef = firebase.database().ref().child("Bonobo");
+    var answer = document.getElementById('myInput');
 
 
 
-        rootRef.on("child_added", snap => {
-            var originalname = snap.child("originalname").val();
-            var latinname = snap.child("latinname").val();
-            var order1 = snap.child("order").val();
-            var family = snap.child("family").val();
-            var genus = snap.child("genus").val();
-            var species = snap.child("species").val();
-            var description = snap.child("description").val();
-
-
-
-                var x = document.createElement("IMG");
-                x.setAttribute("src", "js/elephant.jpg");
-                document.body.appendChild(x);
-
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
-            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
-
-        });
-    }
-
-    else if (answer[answer.selectedIndex].value == "Chimpanzee") {
+    if (answer.value =="Chimpanzee" || answer.value == "Pan" ) {
 
         var rootRef = firebase.database().ref().child("Chimpanzee");
 
@@ -49,24 +16,17 @@ function answers() {
             var family = snap.child("family").val();
             var genus = snap.child("genus").val();
             var species = snap.child("species").val();
-            // var description = snap.child("description").val();
             var description = snap.child("description").val();
-            //var x = document.createElement("IMG");
 
-
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "js/elephant.jpg");
-            document.body.appendChild(x);
-
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
+            $("#table_body1").append("<tr><td>" + description + "</td><td>"+ +"</td><tr>")
             $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
         });
     }
 
-    else if (answer[answer.selectedIndex].value == "Elephnat") {
+    else if (answer.value == "Bonobo" ||answer.value == "Hominidae" || answer.value =="Pan Paniscus"  || answer.value == "Paniscus" ) {
 
-        var rootRef = firebase.database().ref().child("Elephnat");
+        var rootRef = firebase.database().ref().child("Bonobo");
+
 
         rootRef.on("child_added", snap => {
             var originalname = snap.child("originalname").val();
@@ -77,258 +37,10 @@ function answers() {
             var species = snap.child("species").val();
             var description = snap.child("description").val();
 
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "js/elephant.jpg");
-            document.body.appendChild(x);
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
+            $("#table_body1").append("<tr><td>" + description + "</td><td>" + + "</td><tr>")
             $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
+
         });
     }
-    else if (answer[answer.selectedIndex].value == "Horse") {
-
-        var rootRef = firebase.database().ref().child("Horse");
-
-        rootRef.on("child_added", snap => {
-            var originalname = snap.child("originalname").val();
-            var latinname = snap.child("latinname").val();
-            var order1 = snap.child("order").val();
-            var family = snap.child("family").val();
-            var genus = snap.child("genus").val();
-            var species = snap.child("species").val();
-            var description = snap.child("description").val();
-
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "js/elephant.jpg");
-            document.body.appendChild(x);
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
-            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
-        });
-    }
-
-    else if (answer[answer.selectedIndex].value == "Lion") {
-
-        var rootRef = firebase.database().ref().child("Lion");
-
-        rootRef.on("child_added", snap => {
-            var originalname = snap.child("originalname").val();
-            var latinname = snap.child("latinname").val();
-            var order1 = snap.child("order").val();
-            var family = snap.child("family").val();
-            var genus = snap.child("genus").val();
-            var species = snap.child("species").val();
-            var description = snap.child("description").val();
-
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "js/elephant.jpg");
-            document.body.appendChild(x);
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
-
-
-            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
-        });
-    }
-
-    else if (answer[answer.selectedIndex].value == "Panda") {
-
-        var rootRef = firebase.database().ref().child("Panda");
-
-        rootRef.on("child_added", snap => {
-            var originalname = snap.child("originalname").val();
-            var latinname = snap.child("latinname").val();
-            var order1 = snap.child("order").val();
-            var family = snap.child("family").val();
-            var genus = snap.child("genus").val();
-            var species = snap.child("species").val();
-            var description = snap.child("description").val();
-
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "js/elephant.jpg");
-            document.body.appendChild(x);
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
-
-            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
-        });
-    }
-
-    else if (answer[answer.selectedIndex].value == "Penguin") {
-
-        var rootRef = firebase.database().ref().child("Penguin");
-
-        rootRef.on("child_added", snap => {
-            var originalname = snap.child("originalname").val();
-            var latinname = snap.child("latinname").val();
-            var order1 = snap.child("order").val();
-            var family = snap.child("family").val();
-            var genus = snap.child("genus").val();
-            var species = snap.child("species").val();
-            var description = snap.child("description").val();
-
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "js/elephant.jpg");
-            document.body.appendChild(x);
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
-
-
-            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
-        });
-    }
-
-    else if (answer[answer.selectedIndex].value == "Python") {
-
-        var rootRef = firebase.database().ref().child("Python");
-
-        rootRef.on("child_added", snap => {
-            var originalname = snap.child("originalname").val();
-            var latinname = snap.child("latinname").val();
-            var order1 = snap.child("order").val();
-            var family = snap.child("family").val();
-            var genus = snap.child("genus").val();
-            var species = snap.child("species").val();
-            var description = snap.child("description").val();
-
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "js/elephant.jpg");
-            document.body.appendChild(x);
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
-
-
-            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
-        });
-    }
-
-    else if (answer[answer.selectedIndex].value == "Tiger") {
-
-        var rootRef = firebase.database().ref().child("Tiger");
-
-        rootRef.on("child_added", snap => {
-            var originalname = snap.child("originalname").val();
-            var latinname = snap.child("latinname").val();
-            var order1 = snap.child("order").val();
-            var family = snap.child("family").val();
-            var genus = snap.child("genus").val();
-            var species = snap.child("species").val();
-            var description = snap.child("description").val();
-
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "js/elephant.jpg");
-            document.body.appendChild(x);
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
-
-
-            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
-        });
-    }
-
-    else if (answer[answer.selectedIndex].value == "Turtle") {
-
-        var rootRef = firebase.database().ref().child("Turtle");
-
-        rootRef.on("child_added", snap => {
-            var originalname = snap.child("originalname").val();
-            var latinname = snap.child("latinname").val();
-            var order1 = snap.child("order").val();
-            var family = snap.child("family").val();
-            var genus = snap.child("genus").val();
-            var species = snap.child("species").val();
-            var description = snap.child("description").val();
-
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "js/elephant.jpg");
-            document.body.appendChild(x);
-
-            $("#table_body1").append("<tr><td>" + description + "</td><td>"+x+"</td><tr>")
-
-
-            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
-        });
-    }
-
-
-
-}
-
-
-function answersnew() {
-    var answer = document.getElementById("mySelect1");
-
-    if (answer[answer.selectedIndex].value == "Battersea") {
-
-        var rootRef = firebase.database().ref().child("Battersea");
-
-        rootRef.on("child_added", snap => {
-            var address = snap.child("address").val();
-            var phoneno = snap.child("phoneno").val();
-            var time1 = snap.child("time").val();
-
-
-            $("#table_body1").append("<tr><td>" + address + "</td><td>" + phoneno + "</td><td>" + time1 + "</td></tr>")
-        });
-    }
-
-    if (answer[answer.selectedIndex].value == "Bristol") {
-
-        var rootRef = firebase.database().ref().child("Bristol");
-
-        rootRef.on("child_added", snap => {
-            var address = snap.child("address").val();
-            var phoneno = snap.child("phoneno").val();
-            var time1 = snap.child("time").val();
-
-
-            $("#table_body1").append("<tr><td>" + address + "</td><td>" + phoneno + "</td><td>" + time1 + "</td></tr>")
-        });
-    }
-
-    if (answer[answer.selectedIndex].value == "Edinburgh") {
-
-        var rootRef = firebase.database().ref().child("Edinburgh");
-
-        rootRef.on("child_added", snap => {
-            var address = snap.child("address").val();
-            var phoneno = snap.child("phoneno").val();
-            var time1 = snap.child("time").val();
-
-
-            $("#table_body1").append("<tr><td>" + address + "</td><td>" + phoneno + "</td><td>" + time1 + "</td></tr>")
-        });
-    }
-
-    if (answer[answer.selectedIndex].value == "Hamerton") {
-
-        var rootRef = firebase.database().ref().child("Hamerton");
-
-        rootRef.on("child_added", snap => {
-            var address = snap.child("address").val();
-            var phoneno = snap.child("phoneno").val();
-            var time1 = snap.child("time").val();
-
-
-            $("#table_body1").append("<tr><td>" + address + "</td><td>" + phoneno + "</td><td>" + time1 + "</td></tr>")
-        });
-    }
-
-    if (answer[answer.selectedIndex].value == "London") {
-
-        var rootRef = firebase.database().ref().child("London");
-
-        rootRef.on("child_added", snap => {
-            var address = snap.child("address").val();
-            var phoneno = snap.child("phoneno").val();
-            var time1 = snap.child("time").val();
-
-
-            $("#table_body1").append("<tr><td>" + address + "</td><td>" + phoneno + "</td><td>" + time1 + "</td></tr>")
-        });
-    }
-
-
-
 
 }
--- builds/menageriezoo/js/trial.js
+++ builds/menageriezoo/js/trial.js
@@ -1,19 +1,45 @@
+function answers() {
 
-var config = {
-    apiKey: "AIzaSyAGOAeE7W8gWftPStV2nf4PwYyaEuxERNk",
-    authDomain: "animalinformation-cd765.firebaseapp.com",
-    databaseURL: "https://animalinformation-cd765.firebaseio.com",
-    projectId: "animalinformation-cd765",
-    storageBucket: "animalinformation-cd765.appspot.com",
-    messagingSenderId: "843766532952"
-};
+
+    var answer = document.getElementById('myInput');
 
 
 
-new Firebase("https://animalinformation-cd765.firebaseio.com/Bonobo/Bonobo").once('value', show);
+    if (answer.value == "Chimpanzee" ) {
 
-function show(snap) {
-    $('pre').text(JSON.stringify(snap.val(), null));
+        var rootRef = firebase.database().ref().child("Chimpanzee");
+
+        rootRef.on("child_added", snap => {
+            var originalname = snap.child("originalname").val();
+            var latinname = snap.child("latinname").val();
+            var order1 = snap.child("order").val();
+            var family = snap.child("family").val();
+            var genus = snap.child("genus").val();
+            var species = snap.child("species").val();
+            var description = snap.child("description").val();
+
+             $("#table_body1").append("<tr><td>" + description + "</td><td>"+ +"</td><tr>")
+            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
+        });
+    }
+   else if (answer.value == "Bonobo" ) {
+
+        var rootRef = firebase.database().ref().child("Bonobo");
+
+
+        rootRef.on("child_added", snap => {
+            var originalname = snap.child("originalname").val();
+            var latinname = snap.child("latinname").val();
+            var order1 = snap.child("order").val();
+            var family = snap.child("family").val();
+            var genus = snap.child("genus").val();
+            var species = snap.child("species").val();
+            var description = snap.child("description").val();
+
+             $("#table_body1").append("<tr><td>" + description + "</td><td>" + + "</td><tr>")
+            $("#table_body").append("<tr><td>" + originalname + "</td><td>" + latinname + "</td><td>" + order1 + "</td><td>" + family + "</td><td>" + genus + "</td><td>" + species + "</td></tr>")
+
+        });
+    }
+
 }
-
-
